5aaee5e68373fac4e6179c0f66cae961944d954c
Alexandre Magno <alexandre.302@hotmail.com>
2020-01-16 16:54:29 -0300

    try to add pages

---
+++ src/pages/feed/index.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { StatusBar,  StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StackActions, NavigationActions } from 'react-navigation';
+
+import api from '../../services/api';
+import { RNCamera } from 'react-native-camera';
+
+import {
+  Container,
+  Logo,
+  Input,
+  ErrorMessage,
+  Button,
+  ButtonText,
+  SignUpLink,
+  SignUpLinkText,
+  Subtitle,
+} from './styles';
+
+export default class Feed extends Component {
+  static navigationOptions = {
+    headerShown: false,
+  };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+      dispatch: PropTypes.func,
+    }).isRequired,
+  };
+
+  takePicture = async () => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options)
+      alert(data.uri);
+    }
+  }
+
+  render() {
+    return (
+      <Container>
+        <StatusBar hidden />
+        <Logo source={require('../../images/logo-light-green.png')} resizeMode="contain" />
+        <Subtitle>For coffee lovers</Subtitle>
+        <View style={styles.container}>
+          <RNCamera
+            ref={camera => {
+              this.camera = camera;
+            }}
+            style={styles.preview}
+            type={RNCamera.Constants.Type.back}
+            autoFocus={RNCamera.Constants.AutoFocus.on}
+            flashMode={RNCamera.Constants.FlashMode.off}
+            permissionDialogTitle={"Permission to use camera"}
+            permissionDialogMessage={
+              "We need your permission to use your camera phone"
+            }
+          />
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={this.takePicture} style={styles.capture}>
+              <Text style={styles.buttonText}> SNAP </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "black"
+  },
+  preview: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  buttonContainer: {
+    flex: 0,
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: "center",
+    margin: 20
+  },
+  buttonText: {
+    fontSize: 14
+  }
+});
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,18 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator} from 'react-navigation-stack';
 
 import SignIn from './pages/signIn';
- import SignUp from './pages/signUp';
+import SignUp from './pages/signUp';
 import Main from './pages/main';
+import Feed from './pages/feed';
+import AddCoffee from './pages/add-coffee';
+// import Account from './pages/main';
 
 const Routes = createStackNavigator({
 SignIn,
- SignUp,
-Main
+SignUp,
+Main,
+Feed,
+AddCoffee,
 });
 
 const RoutesCtn = createAppContainer(Routes);
